157845955e21a30c1d45061bafa50624d88f9486
msshahid21 <muhammad.s.shahid@outlook.com>
2022-07-30 15:27:18 -0400

    Create 300 pixel radius circle around Central LA

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,6 +4,13 @@ let map = L.map('map', {
     zoom: 4
 });
 
+// Add a marker to the map for Los Angeles, California
+L.circleMarker([34.0522, -118.2437], {
+    color: "black",
+    fillColor: "lightyellow",
+    radius: 300
+}).addTo(map);
+
 // We create the tile layer that will be the background of our map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
