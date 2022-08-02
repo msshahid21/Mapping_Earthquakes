f4880054801d72257528913d816dad02a20d0b1b
msshahid21 <muhammad.s.shahid@outlook.com>
2022-08-02 00:06:10 -0400

    Plot Earthquake points and size by magnitude

--- Earthquakes_past7days/static/js/logic.js
+++ Earthquakes_past7days/static/js/logic.js
@@ -31,24 +31,39 @@ let map = L.map('map', {
 // Pass our map layers into our layers control and add the layers control to the map
 L.control.layers(baseMaps).addTo(map);
 
-// Create a style for the lines
-let myStyle = {
-    color: "blue",
-    weight: 1,
-    fillColor: "yellow"
-}
-
 // Grabbing our GeoJSON data
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 
-    // Creating a GeoJSON layer with the retrieved data
-    L.geoJSON(data).addTo(map);
+    // This function returns the style data for each of the earthquakes we plot on
+    // the map. We pass the magnitude of the earthquake into a function
+    // to calculate the radius.
+    function styleInfo(feature) {
+        return {
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: "#ffae42",
+          color: "#000000",
+          radius: getRadius(feature.properties.mag),
+          stroke: true,
+          weight: 0.5
+        };
+    }
 
-    // // Creating a GeoJSON layer with the retrieved data
-    // L.geoJSON(data, {
-    //     style: myStyle,
-    //     onEachFeature: function(data, layer) {
-    //         layer.bindPopup(`<h2> Neighborhood: ${data.properties.AREA_NAME} </h2>`);
-    //     }
-    // }).addTo(map);
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+            return magnitude * 4;
+    }
+
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJSON(data, {
+        style: styleInfo,
+        pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        }
+    }).addTo(map);
 });
