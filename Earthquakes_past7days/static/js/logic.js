af3b684f0734bbb9a8a88ed366bae4b876ca6b9b
msshahid21 <muhammad.s.shahid@outlook.com>
2022-08-02 00:21:39 -0400

    Add Popup messages for magnitude and location of earthquake

--- Earthquakes_past7days/static/js/logic.js
+++ Earthquakes_past7days/static/js/logic.js
@@ -41,12 +41,32 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return {
           opacity: 1,
           fillOpacity: 1,
-          fillColor: "#ffae42",
+          fillColor: getColor(feature.properties.mag),
           color: "#000000",
           radius: getRadius(feature.properties.mag),
           stroke: true,
           weight: 0.5
         };
+    }
+
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+        if (magnitude > 5) {
+        return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+        return "#ea822c";
+        }
+        if (magnitude > 3) {
+        return "#ee9c00";
+        }
+        if (magnitude > 2) {
+        return "#eecc00";
+        }
+        if (magnitude > 1) {
+        return "#d4ee00";
+        }
+        return "#98ee00";
     }
 
     // This function determines the radius of the earthquake marker based on its magnitude.
@@ -64,6 +84,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         pointToLayer: function(feature, latlng) {
             console.log(data);
             return L.circleMarker(latlng);
+        },
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
         }
     }).addTo(map);
 });
